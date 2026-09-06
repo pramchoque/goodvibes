f082a812a413ffd8ad335c81cdc9825ede0a012f
pramchoque <paramukti@gmail.com>
2026-09-06 22:03:52 +0700

    Update konfigurasi.js

--- konfigurasi.js
+++ konfigurasi.js
@@ -6,5 +6,5 @@
    ========================================================== */
 
 window.ADMIN_KEY = "gantikatasandiini";
-window.SUPABASE_URL = "https://kllirzapjvyhiicpwfog.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://kllirzapjvyhiicpwfog.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsbGlyemFwanZ5aGlpY3B3Zm9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3MDA1ODgsImV4cCI6MjEwNDI3NjU4OH0.wd9CZbca2AFmVWKKYRAHAYVSm1xAcz53aA8hig1XRIQ";
